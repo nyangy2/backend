8211c5fd4c37999c7ac6bb3362fbb0f5997871ac
nyangy2 <bumjum14@naver.com>
2025-04-15 01:05:16 +0900

    스키마 auth.py 수정

--- app/schemas/auth.py
+++ app/schemas/auth.py
@@ -1,9 +1,16 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, EmailStr
 
-class UserCreate(BaseModel):
-    email: str
+# 회원가입 요청 모델
+class SignupRequest(BaseModel):
+    email: EmailStr
     password: str
 
-class Token(BaseModel):
+# 로그인 요청 모델
+class LoginRequest(BaseModel):
+    email: EmailStr
+    password: str
+
+# 로그인 응답 (JWT 토큰)
+class TokenResponse(BaseModel):
     access_token: str
     token_type: str = "bearer"
