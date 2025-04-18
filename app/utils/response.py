b46ef083fadcffd3bb3b70fee40fea9335d7e5dc
nyangy2 <bumjum14@naver.com>
2025-04-18 10:14:41 +0900

    utc 마저 수정

--- app/utils/response.py
+++ app/utils/response.py
@@ -1,10 +1,10 @@
 from fastapi.responses import JSONResponse
 from fastapi import HTTPException
-from datetime import datetime
+from datetime import datetime, timezone
 
 def standard_response(result=None, code="COMMON200", message="요청에 성공하였습니다."):
     return JSONResponse(content={
-        "timestamp": datetime.utcnow().isoformat(),
+        "timestamp": datetime.now(timezone.utc).isoformat(),
         "code": code,
         "message": message,
         "result": result
@@ -14,7 +14,7 @@ def standard_exception(status_code: int, code: str, message: str):
     raise HTTPException(
         status_code=status_code,
         detail={
-            "timestamp": datetime.utcnow().isoformat(),
+            "timestamp": datetime.now(timezone.utc).isoformat(),
             "code": code,
             "message": message,
             "result": None
