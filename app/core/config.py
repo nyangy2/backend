769033fb760cc26adfa5fa00391157ea7cc5fe1d
nyangy2 <bumjum14@naver.com>
2025-04-19 14:38:32 +0900

    core/config.py 수정

--- app/core/config.py
+++ app/core/config.py
@@ -8,8 +8,10 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     DATABASE_URL: str
+    MFDS_API_KEY: str
 
     class Config:
         env_file = ".env"
+        extra = "forbid"
 
 settings = Settings()
