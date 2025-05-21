69876c61ad7ed054d6d20f5257e60e3d8c91c390
nyangy2 <bumjum14@naver.com>
2025-05-21 23:25:03 +0900

    drug_info 추가

--- app/api/router.py
+++ app/api/router.py
@@ -1,5 +1,5 @@
 from fastapi import APIRouter, Depends
-from app.api.endpoints import auth, auth_social, symptoms, image, user_health, mypage, medication
+from app.api.endpoints import auth, auth_social, symptoms, image, user_health, mypage, medication, drug_info
 from app.core.security import get_current_user
 
 api_router = APIRouter()
@@ -24,7 +24,7 @@ api_router.include_router(
     mypage.router, prefix="/mypage", tags=["mypage"],
     dependencies=[Depends(get_current_user)])
 
-#api_router.include_router(drugs.router, prefix="/drugs", tags=["drugs"])
+api_router.include_router(drug_info.router, prefix="/drug_info", tags=["drug_info"])
 
 api_router.include_router(medication.router, prefix="/medications", tags=["medications"])
 
